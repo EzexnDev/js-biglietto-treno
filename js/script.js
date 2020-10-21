3c7052adc2c0a871d843df506e1d584346ffbfe1
Gianmarco <gianmarco.cavalieredev@gmail.com>
2020-10-21 17:23:17 +0200

    Esercizio Finito

--- js/script.js
+++ js/script.js
@@ -4,27 +4,37 @@ var seniorAgeDiscount = 0.4;
 var generateEl = document.getElementById("generate");
 var undoEl = document.getElementById("undo");
 
+
+// Generate Function
 generateEl.addEventListener('click',
-  function() {
-var tripKm = document.getElementById("kmToRun").value;
-var age = document.getElementById("age").value;
-console.log(age);
-console.log(tripKm);
-var finalPrice = tripKm * priceForKM;
-  if (age = "Minorenne") {
-    finalPrice = finalPrice - (finalPrice * underAgeDiscount)
-    document.getElementById("finalPrice").innerHTML = finalPrice + " €"
-    console.log("ao")
-} else if (age = "Adulto") {
-    document.getElementById("finalPrice").innerHTML = finalPrice + " €"
-    console.log("ae")
-} else if (age = "Over65"){
-    finalPrice = finalPrice - (finalPrice * seniorAgeDiscount)
-    document.getElementById("finalPrice").innerHTML = finalPrice + " €"
-    console.log("ababab")
-}
-})
+  function () {
+    var name = document.getElementById("name").value;
+    var tripKm = document.getElementById("kmToRun").value;
+    var age = document.getElementById("age").value;
+    var finalPrice = tripKm * priceForKM;
 
 
+    if (age == "Minorenne") {
+      finalPrice = finalPrice - (finalPrice * underAgeDiscount)
+    } else if (age == "Adulto") {
+    } else if (age == "Over65") {
+      finalPrice = finalPrice - (finalPrice * seniorAgeDiscount)
+    }
 
+    document.getElementById("nameResume").innerHTML = name;
+    document.getElementById("offerta").innerHTML = age;
+    document.getElementById("carrozza").innerHTML= Math.floor(Math.random() * (16 - 1)) + 1; 
+    document.getElementById("cpCode").innerHTML= Math.floor(Math.random() * (10000 - 9000)) + 9000; 
+    document.getElementById("price").innerHTML = finalPrice + " €"
 
+  }
+  )
+
+  // Undo Function
+  undoEl.addEventListener("click",
+  function(){
+    document.getElementById('name').value = "";
+    document.getElementById('kmToRun').value = "";
+    document.getElementById('age').value = "Minorenne"
+  }
+  )
